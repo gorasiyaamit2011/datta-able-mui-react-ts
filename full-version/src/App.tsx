99bf954efa261336f231b13702c69c034528b273
amit-gorasiya <gorasiyaamit2011@gmail.com>
2022-08-03 23:54:41 +0530

    add - feather icons

--- full-version/src/App.tsx
+++ full-version/src/App.tsx
@@ -1,5 +1,15 @@
+import * as Icon from 'react-feather';
+
 function App() {
-  return <h1>App</h1>;
+  return (
+    <>
+      <h1>Icons Feather</h1>
+      <Icon.Activity />
+      <Icon.Camera color="blue" size={45} />
+      <Icon.Camera color="red" />
+      <Icon.Home />
+    </>
+  );
 }
 
 export default App;
